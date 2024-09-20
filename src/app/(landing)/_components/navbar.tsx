a3171d999c645d4f4ad282d918320c6f63417db4
Makdoom Shaikh <makshaikh99@gmail.com>
2024-09-20 23:19:31 +0530

    Add responsiveness in landing page

--- src/app/(landing)/_components/navbar.tsx
+++ src/app/(landing)/_components/navbar.tsx
@@ -1,14 +1,37 @@
 import { Button } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { RiMenu5Line } from "react-icons/ri";
 
 const Navbar = () => {
   return (
     <div className="fixed top-0 w-full bg-white p-3 px-4 border-b felx items-center z-10">
       <div className="md:max-w-screen-2xl mx-auto flex items-center justify-between ">
         <h1 className="text-xl font-bold tracking-wide">Taskflow</h1>
-        <div className="space-x-4 flex items-center justify-between w-full md:block md:w-auto">
+        <div className="hidden space-x-4 w-full md:block md:w-auto">
           <Button variant="secondary">Signup</Button>
           <Button>Login</Button>
         </div>
+        <Drawer>
+          <DrawerTrigger className="md:hidden">
+            <RiMenu5Line className="size-6 block md:hidden cursor-pointer" />
+          </DrawerTrigger>
+          <DrawerContent className="px-4 space-y-8 pb-4">
+            <DrawerTitle className="text-lg text-center text-muted-foreground">
+              Login/Signup to continue
+            </DrawerTitle>
+            <DrawerDescription className="hidden" />
+            <div className="flex flex-col w-full space-y-4">
+              <Button variant="secondary">Signup</Button>
+              <Button>Login</Button>
+            </div>
+          </DrawerContent>
+        </Drawer>
       </div>
     </div>
   );
--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -6,8 +6,8 @@ import { FaGithub } from "react-icons/fa";
 const LandingPage = () => {
   return (
     <div className="h-screen">
-      <BackgroundBeamsWithCollision className="!h-screen flex flex-col">
-        <h2 className="text-2xl relative z-20 md:text-4xl lg:text-7xl font-bold text-center text-black dark:text-white font-sans tracking-tight">
+      <BackgroundBeamsWithCollision className="!h-screen flex flex-col px-2">
+        <h2 className="text-2xl sm:text-3xl relative z-20 md:text-4xl lg:text-7xl font-bold text-center text-black dark:text-white font-sans tracking-tight">
           <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
             <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
               <span className="lg:text-6xl">
@@ -21,9 +21,10 @@ const LandingPage = () => {
             </div>
           </div>
         </h2>
-        <p className="text-2xl text-center mt-3 text-secondary-foreground">
+        <p className="text-md sm:md-lg md:text-2xl text-center mt-3 text-secondary-foreground">
           Project management tool designed to streamline task organization,{" "}
-          <br /> enhance team collaboration, and boost productivity
+          <br className="hidden md:block" /> enhance team collaboration, and
+          boost productivity
         </p>
 
         <div className="space-x-4">
