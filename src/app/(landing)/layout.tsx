17f81f5aecf3b15677825a42b7e43bdb46b6d4fd
Makdoom Shaikh <makshaikh99@gmail.com>
2024-09-20 21:52:07 +0530

    Add navbar

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -1,9 +1,11 @@
 import { ReactNode } from "react";
+import Navbar from "./_components/navbar";
 
 const LandingLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="h-full">
       {/* <Navbar goes here*/}
+      <Navbar />
       <main>{children}</main>
     </div>
   );
